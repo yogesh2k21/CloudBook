ee72af6ff2fe0d83ede7b865c5b903ece2658b8c
Yogesh <52989607+yogesh2k21@users.noreply.github.com>
2021-10-03 12:49:25 +0530

    last commit

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,15 +6,15 @@ function Alert(props) {
     <div id="alertComp" className="w-full" style={{height:'45px'}}>
       {props.alert && (
         <div
-          class={`bg-${props.alert.color}-200 px-4 py-2 ounded-md text-lg flex items-center mx-auto`}
+          className={`bg-${props.alert.color}-200 px-4 py-2 ounded-md text-lg flex items-center mx-auto`}
         >
           <svg
             viewBox="0 0 24 24"
-            class={`text-${props.alert.color}-600 w-5 h-5 sm:w-5 sm:h-5 mr-3`}
+            className={`text-${props.alert.color}-600 w-5 h-5 sm:w-5 sm:h-5 mr-3`}
           >
             <path fill="currentColor" d={props.alert.icon}></path>
           </svg>
-          <span class={`text-${props.alert.color}-800`}>{props.alert.msg}</span>
+          <span className={`text-${props.alert.color}-800`}>{props.alert.msg}</span>
         </div>
       )}
       </div>
